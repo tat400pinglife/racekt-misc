7bfd5ddf35b5085ed48f09dea90e65c5992f966b
tat400pinglife <dzhang765@gmail.com>
2024-12-04 11:27:52 -0500

    classical recursion v2

--- lcs problem demo.rkt
+++ lcs problem demo.rkt
@@ -20,71 +20,51 @@
   "goodbye world")
 
 (define s5
-  "abcd")
+  "abcdeb")
 
 (define s6
+  "ecdaba")
+
+(define s7
+  "abcd")
+
+(define s8
   "bcda")
 
 (define lcs
   (lambda (s1 s2)
-   (define help
-    (lambda (s1 s2 rsf l)
-      (cond ((or (null? s1) (null? s2))  l)
-            ((eq? (car s1) (car s2)) (help (cdr s1) (cdr s2) (append rsf (list(car s1))) (+ l 1)))
-            (else (max (help (cdr s1) s2 rsf l) (help s1 (cdr s2) rsf l)))
-            )))
-    (help (string->list s1) (string->list s2) '() 0)
-    )
-            
-  )
+    (define help
+      (lambda (s1 s2 lcs l)
+        (cond
+          ;; Base case: if either string is empty, return the current length and LCS
+          ((or (null? s1) (null? s2))
+           (cons l lcs))
+
+          ;; If characters match, add to the LCS and continue with the rest
+          ((eq? (car s1) (car s2))
+           (help (cdr s1) (cdr s2) (append lcs (list (car s1))) (+ l 1)))
+
+          ;; If characters don’t match, explore both possibilities
+          (else
+           (let* ((result1 (help (cdr s1) s2 lcs l))
+                  (result2 (help s1 (cdr s2) lcs l)))
+             (if (> (car result1) (car result2))
+                 result1
+                 result2))))))
+    
+    ;; Call the helper function and get the result
+    (let ((result (help (string->list s1) (string->list s2) '() 0)))
+      ;; Display the LCS and its length
+      (display result)
+      (display "LCS: ")
+      (display (list->string (cdr result)))
+      (newline)
+      (display "Length: ")
+      (display (car result))
+      (newline))))
+
 
 (define lcs2
-  (lambda (s1 s2)
-    (let* ((len1 (string-length s1))
-           (len2 (string-length s2))
-           (dp (make-vector (+ len1 1))))
-      ;; Initialize the DP table
-      (let loop1 ((i 0))
-        (if (<= i len1)
-            (begin
-              (vector-set! dp i (make-vector (+ len2 1) 0))
-              (loop1 (+ i 1)))))
-      
-      ;; Fill the DP table
-      (let loop2 ((i 1))
-        (if (<= i len1)
-            (begin
-              (let loop3 ((j 1))
-                (if (<= j len2)
-                    (begin
-                      (let ((char1 (string-ref s1 (- i 1)))
-                            (char2 (string-ref s2 (- j 1))))
-                        (vector-set! (vector-ref dp i) j
-                                     (if (char=? char1 char2)
-                                         (+ 1 (vector-ref (vector-ref dp (- i 1)) (- j 1)))
-                                         (max (vector-ref (vector-ref dp (- i 1)) j)
-                                              (vector-ref (vector-ref dp i) (- j 1))))))
-                      (loop3 (+ j 1)))))
-              (loop2 (+ i 1)))))
-      
-      ;; Function to backtrack and find one LCS
-      (define backtrack
-        (lambda (i j)
-          (if (or (= i 0) (= j 0))
-              ""
-              (let ((char1 (string-ref s1 (- i 1)))
-                    (char2 (string-ref s2 (- j 1))))
-                (if (char=? char1 char2)
-                    (string-append (backtrack (- i 1) (- j 1)) (string char1))
-                    (if (> (vector-ref (vector-ref dp (- i 1)) j)
-                           (vector-ref (vector-ref dp i) (- j 1)))
-                        (backtrack (- i 1) j)
-                        (backtrack i (- j 1))))))))
-      
-      ;; Return the DP table and one LCS
-      (list dp (backtrack len1 len2)))))
-
-(define lcs3
   (lambda (s1 s2)
     (let ((memo '())) ;; Initialize memoization table as an empty alist
       ;; Memoized helper function
